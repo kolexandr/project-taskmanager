26a0bb5fcbc42fe112e01adf30310cdaafeb3ca3
Oleksandr Koniukh <kolexandr1983@gmail.com>
2025-12-21 14:08:50 +0200

    adding mobile navigation

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="text-black flex-col wd-full min-h-screen p-24 align-center justify-center">
       <center>
         <h1 className='font-bold flex-col text-4xl'>Welcome to the <span className='text-orange-400'>Task Manager</span> created by <span className='text-blue-950'>Oleksandr Koniukh!</span></h1>
-        <p className='mt-4 font-light'>To get started, please navigate to the "Create Task" page using the sidebar on the left. There, you can add new tasks to your task list.</p>
+        <p className='mt-4 font-light'>To get started, please navigate to the "Create Task" page using the sidebar on the right. There, you can add new tasks to your task list.</p>
         <p className='mt-4 font-light'>Once you've added some tasks, head over to the "View Tasks" page to see your list of tasks. You can mark tasks as completed or delete them as needed.</p>
         <div className='mt-6'>
           <span className='font-medium'>Happy task managing!)</span>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,8 @@ const Nav = () => {
   const {data: session} = useSession();
   const [providers, setProviders] = useState(null);
 
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
   useEffect(() => {
     // avoid shadowing the state setter `setProviders`
     const setUpProviders = async () => {
@@ -23,13 +25,6 @@ const Nav = () => {
 
   return (
     <nav className="flex w-full pt-3 justify-end ">
-      
-      {/* <Link href="/" className="flex gap-2 flex-center">
-        
-        
-      
-      </Link> */}
-
 
       <div className="sm:flex hidden">
 
@@ -67,6 +62,73 @@ const Nav = () => {
               />
             </Link>
 
+          </div>
+        ) : (
+          <>
+            {providers && Object.values(providers).map((provider) => (
+              <button 
+                type="button"
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className="bg-black text-white mr-5 px-5 py-1.5 rounded-full cursor-pointer hover:bg-gray-800"
+              >
+                Sign In
+              </button>
+            ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile nav */}
+      <div className="sm:hidden flex relative">
+        <Link href='/'>
+          <Image
+            src="/navigation.png"
+            alt="navigation_logo"
+            width={40}
+            height={40}
+            className="fixed top-4 left-4 rounded-full shadow-lg z-50"
+          />
+        </Link>
+        {session?.user ? (
+          <div className="flex">
+            <Image
+              src={session?.user.image}
+              alt="profile"
+              width={40}
+              height={40}
+              className="rounded-full"
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+
+            {toggleDropdown && (
+              <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-50">
+                <Link 
+                  href='/view-tasks'
+                  className="block px-4 py-2 text-black hover:bg-gray-200 text-sm"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  View Plans
+                </Link>
+                <Link 
+                  href='/create-task'
+                  className="block px-4 py-2 text-black hover:bg-gray-200 text-sm"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Tasks
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="w-full text-left px-4 py-2 text-black hover:bg-gray-200 text-sm"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
